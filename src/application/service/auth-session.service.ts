8453cf8cf071bfaf589af8f8ed22bda3b748a8a7
Thomas Mison <thomas.mison1@gmail.com>
2023-10-16 20:03:06 +0200

    WIP: Auth service implemented with authSession

---
+++ src/application/service/auth-session.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@nestjs/common';
+import { DateTime } from 'luxon';
+
+import { AuthSession } from '../../domain/entity/auth-session.entity';
+import { User } from '../../domain/entity/user.entity';
+import { AuthSessionNotFoundException } from '../../domain/exception/auth-session/auth-session-not-found.exception';
+import { AuthSessionRepository } from '../../infrastructure/repository/auth-session.repository';
+/**
+ * We don't call this service with app because this service is only used by AuthController, it's not a business service
+ */
+@Injectable()
+export class AuthSessionService {
+  constructor(private readonly authSessionRepository: AuthSessionRepository) {}
+
+  async getSessionByRefreshToken(refreshToken: string): Promise<AuthSession> {
+    const authSession =
+      await this.authSessionRepository.getOneByRefreshToken(refreshToken);
+
+    if (authSession.isRevoked) {
+      throw new AuthSessionNotFoundException(
+        `AuthSession with refreshToken ${refreshToken} is revoked`,
+      );
+    }
+
+    if (this.isSessionExpired(authSession)) {
+      throw new AuthSessionNotFoundException(
+        `AuthSession with refreshToken ${refreshToken} is expired`,
+      );
+    }
+
+    return authSession;
+  }
+
+  async createAuthSession(
+    user: User,
+    minutesDuration: number,
+  ): Promise<AuthSession> {
+    const authSession = new AuthSession();
+    authSession.user = user;
+    authSession.refreshToken = this.generateRefreshToken();
+    authSession.expiresAt = this.generateExpirationDate(minutesDuration);
+    authSession.createdAt = new Date();
+
+    return await this.authSessionRepository.save(authSession);
+  }
+
+  async refreshAuthSession(
+    authSession: AuthSession,
+    minutesDuration: number,
+  ): Promise<AuthSession> {
+    authSession.refreshToken = this.generateRefreshToken();
+    authSession.expiresAt = this.generateExpirationDate(minutesDuration);
+
+    return await this.authSessionRepository.save(authSession);
+  }
+
+  async revokeAuthSession(authSession: AuthSession): Promise<AuthSession> {
+    authSession.isRevoked = true;
+    return await this.authSessionRepository.save(authSession);
+  }
+
+  private isSessionExpired(authSession: AuthSession): boolean {
+    return authSession.expiresAt < new Date();
+  }
+
+  private generateRefreshToken(): string {
+    return Math.random().toString(36).substring(2);
+  }
+
+  private generateExpirationDate(minutesDuration: number): Date {
+    const dateNow = DateTime.now();
+    const dateNowPlusDuration = dateNow.plus({ minutes: minutesDuration });
+    return dateNowPlusDuration.toJSDate();
+  }
+}
